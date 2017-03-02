a8e34d5c5bff36a87054b45d8ccab334a61871fa
alan <alwat91@uga.edu>
2017-03-02 10:58:24 -0500

    Created db

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :wines, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :favorites
+  has_many :wines, through: :favorites
+  has_many :posts, through: :favorites
+end
