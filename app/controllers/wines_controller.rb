280c3d6c4261868c96b875f44f8a3deeed696a40
alan <alwat91@uga.edu>
2017-03-02 17:50:17 -0500

    Built wine create route

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,2 +1,19 @@
 class WinesController < ApplicationController
+
+def create
+  puts wine_params
+  # binding.pry
+  wine = current_user.wines.new(wine_params)
+  puts wine
+  if wine.save
+    render json: { status: 201, wine: wine}
+  else
+    render json: { status: 422, user: wine.errors}
+  end
+end
+
+
+def wine_params
+  params.required(:wine).permit(:name, :kind, :country, :region, :rating, :description, :user_id)
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302155603) do
+ActiveRecord::Schema.define(version: 20170302220417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20170302155603) do
 
   create_table "wines", force: :cascade do |t|
     t.string   "name"
-    t.string   "type"
+    t.string   "kind"
     t.string   "country"
     t.string   "region"
     t.integer  "rating"
