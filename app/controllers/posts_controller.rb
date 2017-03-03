bb43593d3c8db8581eb4a500dd5b0844020b3760
snfazal <snfazal@gmail.com>
2017-03-03 12:41:04 -0500

    create route posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+
+def create
+  puts post_params
+  posts = current_user.posts.new(posts_params)
+  puts post
+  if post.save
+    render json: { status: 201, post: post}
+  else
+    render json: { status: 422, user: post.errors}
+  end
 end
