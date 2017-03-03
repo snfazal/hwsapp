eea1e1a1b8ab23335984257ab441e4b1d34c9c07
alan <alwat91@uga.edu>
2017-03-03 07:51:46 -0500

    Wine show route complete

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,18 +1,25 @@
 class WinesController < ApplicationController
 
-def create
-  puts wine_params
-  wine = current_user.wines.new(wine_params)
-  puts wine
-  if wine.save
-    render json: { status: 201, wine: wine}
-  else
-    render json: { status: 422, user: wine.errors}
+  def create
+    puts wine_params
+    wine = current_user.wines.new(wine_params)
+    puts wine
+    if wine.save
+      render json: { status: 201, wine: wine}
+    else
+      render json: { status: 422, user: wine.errors}
+    end
   end
-end
 
+  def show
+    render json: { status: 200, wine: Wine.find(params[:id]) }
+  end
 
-def wine_params
-  params.required(:wine).permit(:name, :kind, :country, :region, :rating, :description, :user_id)
-end
+  def index
+    render json: { status: 200, wines: Wine.all }
+  end
+
+  def wine_params
+    params.required(:wine).permit( :name, :kind, :country, :region, :rating, :description, :user_id)
+  end
 end
