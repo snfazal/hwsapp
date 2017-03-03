77f68ecb141f071a9f176da9ee8d34ba72bc9368
alan <alwat91@uga.edu>
2017-03-03 08:24:00 -0500

    Temporarily disabled post favoriting

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
 
 
   has_many :posts, dependent: :destroy
-  has_many :favorited_posts, :through => :favorites, :source => :post
+  # has_many :favorited_posts, :through => :favorites, :source => :post
   has_secure_password
 end
