00958d4a5f783785a438329b513b50facd76c73c
alan <alwat91@uga.edu>
2017-03-05 15:04:12 -0500

    Fixed issue with deleting favorited wines

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,5 +1,5 @@
 class Wine < ApplicationRecord
   belongs_to :user
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favoriting_users, :through => :favorites, :source => :user
 end
