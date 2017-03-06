4e053de798c81b0d38f5e1ea5e17e1e4d87b9af0
alan <alwat91@uga.edu>
2017-03-06 07:51:22 -0500

    Removed uniqueness validation on favorited wines

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :wines, dependent: :destroy
   has_many :favorites
   has_many :favorited_wines, :through =>  :favorites, :source => :wine
-  validates :favorited_wines, uniqueness: true
+  # validates :favorited_wines, uniqueness: true
 
   has_many :posts, dependent: :destroy
   has_secure_password
